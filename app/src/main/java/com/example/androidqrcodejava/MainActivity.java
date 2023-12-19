26f6eb80f99f9198ab0da583ab576ed9a45066e7
Arsa <arsa2170721@gmail.com>
2023-12-19 16:55:51 +0400

    +

--- app/src/main/java/com/example/androidqrcodejava/MainActivity.java
+++ app/src/main/java/com/example/androidqrcodejava/MainActivity.java
@@ -1,13 +1,85 @@
 package com.example.androidqrcodejava;
 
+import android.Manifest.permission;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.view.View;
+import android.widget.Toast;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.content.ContextCompat;
+import com.example.androidqrcodejava.databinding.ActivityMainBinding;
+import com.google.zxing.client.android.Intents;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    showCamera();
+                } else {
+                    //Show why user need permission
+                }
+            });
+
+    private  ActivityResultLauncher<ScanOptions> qrCodeLauncher = registerForActivityResult(new ScanContract(), result->{
+        if (result.getContents() == null) {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+        } else {
+            setResult(result.getContents());
+        }
+    });
+
+    private void setResult(String contents) {
+        binding.textResult.setText(contents);
+    }
+
+    private void showCamera() {
+        ScanOptions options = new ScanOptions();
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE);
+        options.setPrompt("Scan QR code");
+        options.setCameraId(0);
+        options.setBeepEnabled(false);
+        options.setBarcodeImageEnabled(true);
+        options.setOrientationLocked(false);
+
+        qrCodeLauncher.launch(options);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        initBinding();
+        initViews();
+    }
+
+    private void initViews() {
+        binding.fab.setOnClickListener(view -> {
+            checkPermissionAndShowActivity(this);
+        });
+    }
+
+    private void checkPermissionAndShowActivity(Context context) {
+        if(ContextCompat.checkSelfPermission(
+                context,
+                permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED) {
+            showCamera();
+        } else if (shouldShowRequestPermissionRationale(permission.CAMERA)){
+            Toast.makeText(context, "Camera permission required", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermissionLauncher.launch(permission.CAMERA);
+        }
+    }
+
+    private void initBinding() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
